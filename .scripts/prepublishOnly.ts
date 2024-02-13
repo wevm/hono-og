3b31550aed8929c8e0b25d7c4bbce283f42e1dec
jxom <jxom@users.noreply.github.com>
2024-02-13 00:34:03 +0000

    chore: format

--- .scripts/prepublishOnly.ts
+++ .scripts/prepublishOnly.ts
@@ -1,11 +1,11 @@
-import { join } from 'node:path'
+import { join } from "node:path";
 
-const packageJsonPath = join(import.meta.dir, '../src/package.json')
-const packageJson = await Bun.file(packageJsonPath).json()
+const packageJsonPath = join(import.meta.dir, "../src/package.json");
+const packageJson = await Bun.file(packageJsonPath).json();
 
 // NOTE: We explicitly don't want to publish the type field.
 // We create a separate package.json for `dist/cjs` and `dist/esm` that has the type field.
 // biome-ignore lint/performance/noDelete:
-delete packageJson.type
+delete packageJson.type;
 
-Bun.write(packageJsonPath, JSON.stringify(packageJson, null, 2))
+Bun.write(packageJsonPath, JSON.stringify(packageJson, null, 2));
