37abac9a1ec54a26a6e57efe799cd65f50b2a353
moxey.eth <jakemoxey@gmail.com>
2024-03-07 12:39:42 +1100

    feat: support next

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,57 @@ import { type HonoElement, toReactNode } from "./utils.js";
 
 const pkg = Og;
 
+function importModule(): Promise<typeof import("@vercel/og")> {
+  return import(
+    (() => {
+      if (typeof process.env.NEXT_RUNTIME === "undefined") return "@vercel/og";
+      if (process.env.NEXT_RUNTIME === "edge")
+        return "next/dist/compiled/@vercel/og/index.edge.js";
+      return "next/dist/compiled/@vercel/og/index.node.js";
+    })()
+  );
+}
+
 export type ImageResponseOptions = ConstructorParameters<
   typeof Og.ImageResponse
 >[1];
 
-export class ImageResponse extends Og.ImageResponse {
-  constructor(element: HonoElement, options?: ImageResponseOptions) {
-    super(toReactNode(element), options);
+export class ImageResponse extends Response {
+  public static displayName = "ImageResponse";
+  constructor(element: HonoElement, options: ImageResponseOptions = {}) {
+    const readable = new ReadableStream({
+      async start(controller) {
+        const OGImageResponse = (await importModule()).ImageResponse;
+        const imageResponse = new OGImageResponse(
+          toReactNode(element),
+          options,
+        ) as Response;
+
+        if (!imageResponse.body) return controller.close();
+
+        const reader = imageResponse.body!.getReader();
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) {
+            return controller.close();
+          }
+          controller.enqueue(value);
+        }
+      },
+    });
+
+    super(readable, {
+      headers: {
+        "content-type": "image/png",
+        "cache-control":
+          process.env.NODE_ENV === "development"
+            ? "no-cache, no-store"
+            : "public, immutable, no-transform, max-age=31536000",
+        ...options.headers,
+      },
+      status: options.status,
+      statusText: options.statusText,
+    });
   }
 }
 
